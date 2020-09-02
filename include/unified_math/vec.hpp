0c5bd1b40ae999b09475b3db914de91ea61ac652
fewizz <hazeevaidar@gmail.com>
2020-09-02 16:37:33 +0300

    simplify?

--- include/unified_math/vec.hpp
+++ include/unified_math/vec.hpp
@@ -17,173 +17,124 @@
 namespace uni {
 
 template<class T>
-struct possible_vec {
-    struct internal {
-        template<class V0>
-        using size_t = std::tuple_size<V0>;
-    };
-    using size_t = std::experimental::detected_t<internal::template size_t, T>;
+static constexpr bool has_tuple_size_v = std::experimental::is_detected_v<std::tuple_size, T>;
 
-    struct has_size : std::integral_constant<
-        bool,
-        not std::is_same_v<std::experimental::nonesuch, size_t>
-    >{};
+template<class T, std::size_t I>
+using invoke_result_of_get_t = decltype(std::get<I>( std::declval<T>() ));
 
-    static constexpr bool has_size_v = has_size::value;
-
-    template<class...>
-    struct with_size {
-        static_assert(has_size_v, "this type doesn't has std::tuple_size specialisation");
-
-        struct size : std::integral_constant<std::size_t, std::tuple_size_v<T>> {};
-        static constexpr std::size_t size_v = size::value;
-
-        struct internal {
-            template<std::size_t I>
-            struct index {
-                static_assert(I<size_v, "index out of bounds");
-                template<class V0>
-                using get_t = decltype(std::get<I>( std::declval<V0>() ));
-            };
-        };
-
-        template<std::size_t I>
-        using get_t = std::experimental::detected_t<internal::template index<I>:: template get_t, T>;
-
-        template<std::size_t I>
-        using decayed_get_t = std::decay_t<get_t<I>>;
-
-        template<std::size_t I>
-        struct has_get : std::integral_constant<
-            bool,
-            not std::is_same_v<std::experimental::nonesuch, get_t<I>>
-        >{};
-
-        template<std::size_t I>
-        static constexpr bool has_get_v = has_get<I>::value;
-
-        template<std::size_t begin=0, class...>
-        struct has_gets : std::integral_constant<
-            bool,
-            has_get_v<begin> and has_gets<begin+1>::value
-        >{};
-
-        template<class...Unused>
-        struct has_gets<size_v - 1, Unused...> : std::integral_constant< bool, has_get_v<size_v - 1> >{};
-
-        template<class... Unused>
-        static constexpr bool has_gets_v = has_gets<0, Unused...>::value;
-
-        template<class... Unused>
-        struct with_gets {
-            static_assert(has_gets_v<>, "this type doesn't has std::get specialisations for every index");
-
-            template<std::size_t begin=0, class...>
-            struct has_same_decayed_gets : std::integral_constant<
-                bool,
-                std::is_same_v<decayed_get_t<0>, decayed_get_t<begin>>
-                 and has_same_decayed_gets<begin+1>::value
-            >{};
-
-            template<class...Unused0>
-            struct has_same_decayed_gets<size_v - 1, Unused0...>
-            : std::integral_constant< bool, std::is_same_v<get_t<0>, get_t<size_v - 1>> >{};
-
-            static constexpr bool has_same_decayed_gets_v = has_same_decayed_gets<>::value;         
-        };
-
-        template<class...>
-        struct with_same_gets {
-            static_assert(with_gets<>::has_same_decayed_gets_v, "std::get specialisations don't have same RT");
-
-            using get_t = get_t<0>;
-            using decayed_get_t = decayed_get_t<0>;
-
-            template<class T0>
-            struct decayed_get_t_same_as
-            : std::integral_constant<bool, std::is_same_v<T0, decayed_get_t>>{};
-        };
-    };
-
-    template<std::size_t N, bool=has_size_v>
-    struct has_size_of : std::integral_constant<bool, false>{};
-
-    template<std::size_t N>
-    struct has_size_of<N, true> : std::integral_constant<bool, with_size<>::size_v == N>{};
-
-    struct has_size_and_same_decayed_gets {
-        template<bool=has_size_v, class...>
-        struct a : std::integral_constant<bool, false>{};
-        template<class... Unused>
-        struct a<true, Unused...>
-        : std::integral_constant<bool, with_size<>::template has_gets_v<> >{};
-
-        template<bool=a<>::value, class...>
-        struct b : std::integral_constant<bool, false>{};
-        template<class... Unused>
-        struct b<true, Unused...>
-        : std::integral_constant<
-            bool,
-            with_size<>::template with_gets<>::template has_same_decayed_gets<>::value
-        >{};
-
-        static constexpr bool value = b<>::value;
-    };
-
-    static constexpr bool has_size_and_same_decayed_gets_v =
-        has_size_and_same_decayed_gets::value;
+template<class T, std::size_t I>
+class has_get_for_index {
+    template<class T0>
+    using get_t = decltype(std::get<I>( std::declval<T0>() ));
+public:
+    static constexpr bool value = std::experimental::is_detected_v<get_t, T>;
 };
 
+template<class T, std::size_t I>
+static constexpr bool has_get_for_index_v = has_get_for_index<T, I>::value;
 
-template<class V, std::size_t N, class T>
-struct is_vec {
-    using v = possible_vec<V>;
+template<class T, std::size_t I0, std::size_t... Is>
+class has_gets_for_indicies {
+    template<std::size_t _I0, std::size_t _I1, std::size_t... _Is>
+    static constexpr bool v() {
+        return has_get_for_index_v<T, _I0> and has_get_for_index_v<T, _I1>;
+    }
+    template<std::size_t _I0>
+    static constexpr bool v() {
+        return has_get_for_index_v<T, _I0>;
+    }
+public: 
+    static constexpr bool value = v<I0, Is...>();
+};
 
-    template<
-        bool=v::has_size_and_same_decayed_gets_v,
-        class...
-    >
-    struct a : std::integral_constant<bool, false>{};
-    template<class... Unused>
-    struct a<true, Unused...>
-    : std::integral_constant<
+template<class T, std::size_t... Is>
+static constexpr bool has_gets_for_indicies_v = has_gets_for_indicies<T, Is...>::value;
+
+template<class T, std::size_t I0, std::size_t I1, std::size_t... Is>
+class has_same_gets_for_indicies {
+    template<std::size_t _I0, std::size_t _I1, std::size_t _I2, std::size_t... _Is>
+    static constexpr bool v() {
+        return v<_I0, _I1>() and v<_I1, _I2, _Is...>();
+    }
+
+    template<std::size_t _I0, std::size_t _I1>
+    static constexpr bool v() {
+        return std::is_same_v<
+            invoke_result_of_get_t<T, _I0>,
+            invoke_result_of_get_t<T, _I1>
+        >;
+    }
+public: 
+    static constexpr bool value = v<I0, I1, Is...>();
+};
+
+template<class T, std::size_t...Is>
+static constexpr bool has_same_gets_for_indicies_v =
+    has_same_gets_for_indicies<T, Is...>::value;
+
+namespace internal {
+    template<template<class, std::size_t...> class C, class T, std::size_t N>
+    class check_range_for {
+        template<std::size_t... Is>
+        static constexpr bool v(std::index_sequence<Is...>) {
+            return C<T, Is...>::value;
+        }
+    public:
+        static constexpr bool value = v(std::make_index_sequence<N>());
+    };
+}
+
+template<class T, std::size_t S>
+static constexpr bool has_gets_for_size_v =
+    internal::check_range_for<has_gets_for_indicies, T, S>::value;
+
+template<class T, std::size_t S>
+static constexpr bool has_same_gets_for_size_v =
+    internal::check_range_for<has_same_gets_for_indicies, T, S>::value;
+
+template<class T, std::size_t N, class E>
+class is_vec {
+    template<bool=has_tuple_size_v<T> and has_gets_for_size_v<T, N>,class...>
+    struct a:std::integral_constant<bool,false>{};
+    
+    template<class...U> struct a<true,U...>
+    :std::integral_constant<
         bool,
-        v::template with_size<>::size_v == N
-        and v::template with_size<>::template with_same_gets<>::template decayed_get_t_same_as<T>::value
+        has_same_gets_for_size_v<T, N> and
+        std::is_same_v<std::decay_t<decltype(std::get<0>(std::declval<T>()))>, E>
     >{};
 
+    template<class...R>
+    static constexpr bool b() {
+        if constexpr(sizeof...(R) != N) return b<E, R...>();
+        else return std::is_constructible_v<T, R...>;
+    }
+public:
     static constexpr bool value =
-        a<>::value and
-        sizeof(V) ==
-        v::template with_size<>::size_v *
-        sizeof(typename v::template with_size<>::decayed_get_t<0>);
+        a<>::value and b<>()
+        and sizeof(T) == N*sizeof(E);
 };
 
-template<class V, std::size_t D, class T>
-static constexpr bool is_vec_v = is_vec<V, D, T>::value;
+template<class T, std::size_t N, class E>
+static constexpr bool is_vec_v = is_vec<T, N, E>::value;
 
-template<class V, std::size_t D, class T>
-concept vec = is_vec_v<V, D, T>;
+template<class T>
+class is_any_vec {
+    template<class T0>
+    using decayed = std::decay_t<invoke_result_of_get_t<T0, 0>>;
 
-template<class V>
-struct is_any_vec {
-    using v = possible_vec<V>;
-
-    template<
-        bool=v::has_size_and_same_decayed_gets_v,
-        class...
-    >
+    template<bool=has_get_for_index_v<T, 0>, class...>
     struct a : std::integral_constant<bool, false>{};
-    template<class... Unused>
-    struct a<true, Unused...>
-    : std::integral_constant<
-        bool,
-        sizeof(V) ==
-        v::template with_size<>::size_v *
-        sizeof(typename v::template with_size<>::decayed_get_t<0>)
-    >{};
 
+    template<class...U>
+    struct a<true, U...> : std::integral_constant<
+        bool,
+        is_vec_v<T, sizeof(T)/sizeof(decayed<T>), decayed<T>>
+    >{};
+public:
     static constexpr bool value = a<>::value;
 };
 
+template<class T>
+static constexpr bool is_any_vec_v = is_any_vec<T>::value;
 }
--- test.cpp
+++ test.cpp
@@ -2,15 +2,20 @@
 #include <vector>
 #include <string>
 
-#include "include/unified_math/vec2.hpp"
+//#include "include/unified_math/vec2.hpp"
+#include "include/unified_math/vec.hpp"
 #include "glm/vec2.hpp"
 
 using namespace uni;
 using namespace std;
 
 void exec(vector<string> args) {
+    static_assert(!is_vec_v<int, 2, int>);
     static_assert(is_vec_v<std::pair<int, int>, 2, int>);
+    static_assert(!is_vec_v<std::pair<int, int>, 2, float>);
     static_assert(is_vec_v<glm::vec2, 2, float>);
     static_assert(is_vec_v<std::tuple<double, double, double, double>, 4, double>);
-    static_assert(is_any_vec<std::tuple<char, char>>::value);
+    static_assert(is_any_vec_v<std::tuple<char, char>>);
+    static_assert(!is_any_vec_v<int>);
+    static_assert(!is_any_vec_v<std::tuple<int, unsigned, char>>);
 }
