a062eb228a653878f893ad3f68cb1b186394cb1a
fewizz <hazeevaidar@gmail.com>
2023-02-10 14:23:31 +0300

    `clamp`

--- include/math/vector.hpp
+++ include/math/vector.hpp
@@ -2,6 +2,7 @@
 
 #include <array.hpp>
 #include <integer.hpp>
+#include <number.hpp>
 
 namespace math {
 
@@ -19,20 +20,63 @@ namespace math {
 		}
 
 		constexpr auto operator + (Type v) const {
-			vector cpy{ *this };
-			for(auto& v0 : cpy.m_storage) v0 += v;
+			vector cpy { *this };
+			for(auto& v0 : cpy) v0 += v;
+			return cpy;
+		}
+
+		constexpr auto operator + (vector v) const {
+			vector cpy { *this };
+			for(nuint i = 0; i < Size; ++i) {
+				cpy[i] += v[i];
+			}
+			return cpy;
+		}
+
+		constexpr auto operator - (Type v) const {
+			vector cpy { *this };
+			for(auto& v0 : cpy) v0 -= v;
+			return cpy;
+		}
+
+		constexpr auto operator - (vector v) const {
+			vector cpy { *this };
+			for(nuint i = 0; i < Size; ++i) {
+				cpy[i] -= v[i];
+			}
 			return cpy;
 		}
 
 		constexpr auto operator * (Type v) const {
-			vector cpy{ *this };
-			for(auto& v0 : cpy.m_storage) v0 *= v;
+			vector cpy { *this };
+			for(auto& v0 : cpy) v0 *= v;
+			return cpy;
+		}
+
+		constexpr auto operator * (vector v) const {
+			vector cpy { *this };
+			for(nuint i = 0; i < Size; ++i) {
+				cpy[i] *= v[i];
+			}
+			return cpy;
+		}
+
+		constexpr vector operator / (Type v) const {
+			vector cpy { *this };
+			for(auto& v0 : cpy) v0 /= v;
 			return cpy;
 		}
 
 		constexpr vector& operator *= (Type v) {
 			for(auto& v0 : *this) v0 *= v;
 			return *this;
+		}
+
+		constexpr vector clamp(vector min, vector max) {
+			return {
+				number { (*this)[0] }.clamp(min[0], max[0]),
+				number { (*this)[1] }.clamp(min[1], max[1])
+			};
 		}
 
 	};
